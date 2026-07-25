f1a04ed45fac2171fc1125d94e962531eeb239f1
Adenekan Zakariyah <opeyemizakariyah2@gmail.com>
2026-07-25 12:42:54 +0100

    Refine style.css

--- script.js
+++ script.js
@@ -443,6 +443,6 @@ if (savedTheme){
   html.dataset.theme = "light";
 }
 
-const preferDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const preferDarkTheme = window.matchMedia("(prefers-color-scheme: light)").matches;
 console.log(preferDarkTheme);
-html.dataset.theme = preferDarkTheme ? "dark" : "light";
+html.dataset.theme = preferDarkTheme ? "light" : "dark";
